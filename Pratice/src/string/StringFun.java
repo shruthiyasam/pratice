c90adc091efdccf2606882df2b5aa8d0f84a9f35
shruthiyasam <shruthiyasam83@gmail.com>
2019-07-20 10:57:26 +0530

    commit string

---
+++ Pratice/src/string/StringFun.java
@@ -0,0 +1,13 @@
+package string;
+
+public class StringFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="shruthi";
+		System.out.println(str);
+		
+
+	}
+
+}
